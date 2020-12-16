b051233c0c9f0b80584c06c0bb07b3b9ef31da0d
Calvin Wijaya <47976609+crshao@users.noreply.github.com>
2020-12-16 14:31:42 +0700

    little fix

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('layouts.sidebar_template.sidebar_example');
+// Route::get('/', function () {
+//     return view('layouts.sidebar_template.sidebar_example');
+// });
+
+Route::get('/', function() {
+    return view('welcome');
 });
 
 Auth::routes();
