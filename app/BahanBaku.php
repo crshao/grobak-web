446c1cf46c4fb7c8e1883e1b1a8ce819b5aaf577
Calvin Wijaya <47976609+crshao@users.noreply.github.com>
2020-12-26 11:43:45 +0700

    add relationship antara resep dan bahan baku

--- app/BahanBaku.php
+++ app/BahanBaku.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class BahanBaku extends Model
 {
     protected $table = 'bahanbakus';
+
+    //Artinya 1 bahan baku dapat menjadi bagian
+    //dari beberapa resep
+    public function reseps()
+    {
+        return $this->belongsTo(Resep::class);
+    }
 }
--- app/Resep.php
+++ app/Resep.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Resep extends Model
 {
     protected $table = 'reseps';
+
+    //Artinya satu resep punya banyak bahan baku
+    public function bahanbakus()
+    {
+        return $this->hasMany(BahanBaku::class);
+    }
 }
